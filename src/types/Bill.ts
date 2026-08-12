b30dc6d6fac2ace94899b7d4a4f4b40ec35e360f
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-11 22:21:07 -0700

    Update Bill.ts

--- src/types/Bill.ts
+++ src/types/Bill.ts
@@ -1,10 +1,24 @@
 export interface Bill {
   id: string;
+ 
   name: string;
+ 
   amount: number;
+ 
   dueDay: number;
-  paid: boolean;
+ 
   category: string;
-  notes?: string;
+ 
+  paid: boolean;
+ 
+  recurring: boolean;
+ 
+  autoPay: boolean;
+ 
+  notes: string;
+ 
   createdAt: string;
+ 
+  updatedAt?: string;
 }
+ 
