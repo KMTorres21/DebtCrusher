42ec5b7eb7473152ee405d467280c4f0183fa880
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-12 19:15:22 -0600

    Update AppLayout.tsx

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,1 +1,21 @@
+import { Outlet } from "react-router-dom";
 
+import Header from "../components/common/Header";
+import BottomNav from "../components/common/BottomNav";
+import PageContainer from "../components/common/PageContainer";
+
+export default function AppLayout() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex flex-col">
+      <Header />
+
+      <main className="flex-1 pb-24">
+        <PageContainer>
+          <Outlet />
+        </PageContainer>
+      </main>
+
+      <BottomNav />
+    </div>
+  );
+}
