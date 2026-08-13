ee495ddd87c3c3960cd43a6b6d522496edbb20b5
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-13 10:48:27 -0600

    Update AppLayout.tsx

--- src/app/AppLayout.tsx
+++ src/app/AppLayout.tsx
@@ -1,18 +1,19 @@
 import { Outlet } from "react-router-dom";
- 
 import Header from "../components/common/Header";
 import BottomNav from "../components/common/BottomNav";
- 
+
 export default function AppLayout() {
   return (
     <div className="min-h-screen bg-slate-100">
+
       <Header />
- 
-      <main className="pb-24">
+
+      <main className="min-h-[calc(100vh-140px)] pb-24">
         <Outlet />
       </main>
- 
+
       <BottomNav />
+
     </div>
   );
 }
