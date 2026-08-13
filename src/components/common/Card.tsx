b9d69254663cfe7346b7e7095e830925ae6750fe
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-12 20:05:24 -0600

    Update Card.tsx

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,1 +1,23 @@
+import { ReactNode } from "react";
+import clsx from "clsx";
 
+interface CardProps {
+  children: ReactNode;
+  className?: string;
+}
+
+export default function Card({
+  children,
+  className,
+}: CardProps) {
+  return (
+    <div
+      className={clsx(
+        "rounded-2xl bg-white shadow-md p-5",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
