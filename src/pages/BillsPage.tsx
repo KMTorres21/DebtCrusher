f9b4f57830169ba5ce74eef8eafa8d303057c73a
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-13 09:01:36 -0700

    Refactor BillsPage component structure
    Refactor BillsPage component to simplify rendering and remove unused code.

--- src/pages/BillsPage.tsx
+++ src/pages/BillsPage.tsx
@@ -1,28 +1,13 @@
-export default function BillsPage() {
-  const { bills, addBill, deleteBill, togglePaid } = useBills();
- 
-  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
- 
-  return (
-    <>
-      <BillList
-        bills={bills}
+return (
+  <div className="space-y-4">
+    {bills.map((bill) => (
+      <BillCard
+        key={bill.id}
+        bill={bill}
         onTogglePaid={togglePaid}
+        onEdit={handleEdit}
         onDelete={deleteBill}
-        onEdit={(bill) => {
-          // We'll implement this later
-        }}
       />
- 
-      <FloatingActionButton
-        onClick={() => setIsAddModalOpen(true)}
-      />
- 
-      <AddBillModal
-        open={isAddModalOpen}
-        onClose={() => setIsAddModalOpen(false)}
-        onSave={addBill}
-      />
-    </>
-  );
-}
+    ))}
+  </div>
+);
