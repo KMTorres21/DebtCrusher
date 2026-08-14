1f981ed178e70dfacf5c0e7758a7b416eaca617a
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-14 14:49:05 -0600

    Update TimelineEvent.ts

--- src/types/TimelineEvent.ts
+++ src/types/TimelineEvent.ts
@@ -1,1 +1,18 @@
-new
+export type TimelineEventType =
+  | "income"
+  | "bill"
+  | "debt";
+
+export interface TimelineEvent {
+  id: string;
+
+  date: string;
+
+  type: TimelineEventType;
+
+  title: string;
+
+  amount: number;
+
+  runningBalance?: number;
+}
