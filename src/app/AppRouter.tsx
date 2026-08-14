519802927072ce1e3c525fea173622ecc17d7c47
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-14 17:42:24 -0600

    cal page

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -18,6 +18,7 @@ export default function AppRouter() {
           <Route path="/debts" element={<DebtsPage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/timeline" element={<TimelinePage />} />
+          <Route path="/calendar" element={<CalendarPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
