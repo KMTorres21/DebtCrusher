c8a06dfbcb3cdcd0d4ec3ee12321558b3e78c68e
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-14 13:27:41 -0600

    Create PageHeader.tsx

---
+++ src/components/common/PageHeader.tsx
@@ -0,0 +1,23 @@
+interface PageHeaderProps {
+  title: string;
+  subtitle?: string;
+}
+
+export default function PageHeader({
+  title,
+  subtitle,
+}: PageHeaderProps) {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-slate-900">
+        {title}
+      </h1>
+
+      {subtitle && (
+        <p className="mt-2 text-lg text-slate-500">
+          {subtitle}
+        </p>
+      )}
+    </div>
+  );
+}
