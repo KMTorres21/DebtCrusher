ab4378a03a4fe20f968488423e8d3dee033ae900
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-14 14:39:15 -0600

    Update financialEngine.ts

--- src/utils/financialEngine.ts
+++ src/utils/financialEngine.ts
@@ -1,1 +1,43 @@
-new
+import { Bill } from "../types/Bill";
+import { Debt } from "../types/Debt";
+import { Income } from "../types/Income";
+
+export interface FinancialSummary {
+  totalIncome: number;
+  totalBills: number;
+  totalDebtPayments: number;
+  remainingCash: number;
+}
+
+export function calculateFinancialSummary(
+  bills: Bill[],
+  debts: Debt[],
+  income: Income[]
+): FinancialSummary {
+  const totalIncome = income.reduce(
+    (sum, item) => sum + item.amount,
+    0
+  );
+
+  const totalBills = bills.reduce(
+    (sum, bill) => sum + bill.amount,
+    0
+  );
+
+  const totalDebtPayments = debts.reduce(
+    (sum, debt) => sum + debt.minimumPayment,
+    0
+  );
+
+  const remainingCash =
+    totalIncome -
+    totalBills -
+    totalDebtPayments;
+
+  return {
+    totalIncome,
+    totalBills,
+    totalDebtPayments,
+    remainingCash,
+  };
+}
