1200e472519bbade1962c4fc3496e9cf4d7d5347
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-13 21:50:43 -0600

    Update debtStorage.ts

--- src/utils/debtStorage.ts
+++ src/utils/debtStorage.ts
@@ -1,1 +1,24 @@
-new
+import { Debt } from "../types/Debt";
+
+const STORAGE_KEY = "debtcrusher-debts";
+
+export function loadDebts(): Debt[] {
+  const data = localStorage.getItem(STORAGE_KEY);
+
+  if (!data) {
+    return [];
+  }
+
+  try {
+    return JSON.parse(data);
+  } catch {
+    return [];
+  }
+}
+
+export function saveDebts(debts: Debt[]) {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify(debts)
+  );
+}
