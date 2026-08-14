a43def9072f06b059f56c8320904ef42e3e3bade
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-14 11:45:15 -0600

    fix code

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import { formatCurrency } from "../utils/formatCurrency";
-import { UpcomingBills } from "../components/dashboard/UpcomingBills";
+import  UpcomingBills  from "../components/dashboard/UpcomingBills";
 import { useBills } from "../hooks/useBills";
 
 export default function DashboardPage() {
