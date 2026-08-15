f5e9bff3138690a23bda80fbcb5a86b0c2715990
Kendra Millard-Torres <104098744+KMTorres21@users.noreply.github.com>
2026-08-14 19:17:44 -0600

    Create calendarEvents.ts

---
+++ src/utils/calendarEvents.ts
@@ -0,0 +1,47 @@
+import { Bill } from "../types/Bill";
+import { Income } from "../types/Income";
+import { Debt } from "../types/Debt";
+import { CalendarEvent } from "../types/CalendarEvent";
+
+export function buildCalendarEvents(
+  bills: Bill[],
+  income: Income[],
+  debts: Debt[]
+): CalendarEvent[] {
+  const events: CalendarEvent[] = [];
+
+  bills.forEach((bill) => {
+    events.push({
+      id: bill.id,
+      date: bill.dueDate,
+      type: "bill",
+      title: bill.name,
+      amount: bill.amount,
+      paid: bill.paid,
+    });
+  });
+
+  income.forEach((item) => {
+    events.push({
+      id: item.id,
+      date: item.nextPayDate,
+      type: "income",
+      title: item.source,
+      amount: item.amount,
+    });
+  });
+
+  debts.forEach((debt) => {
+    if (debt.paymentDueDate) {
+      events.push({
+        id: debt.id,
+        date: debt.paymentDueDate,
+        type: "debt",
+        title: debt.name,
+        amount: debt.minimumPayment,
+      });
+    }
+  });
+
+  return events.sort((a, b) => a.date.localeCompare(b.date));
+}
