12a7ccb007f1bbcf59eecd94538304cb666789b0
KMillardTorres <kmillard-torres@RepublicServices.com>
2026-08-26 10:51:15 -0700

    added comma

--- src/utils/paydayStrategy.ts
+++ src/utils/paydayStrategy.ts
@@ -991,7 +991,7 @@ function allocateObligations(
         obligation,
         eligibleIndexes,
         paydayPlans,
-        availableCents
+        availableCents,
         settings
       );
     }
