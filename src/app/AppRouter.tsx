5cc5737b13f84491099bb655c67dc5a83c390869
KMillardTorres <kmillard-torres@RepublicServices.com>
2026-09-01 19:18:38 -0700

    fix app router for debt payoff

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -8,6 +8,7 @@ import DebtsPage from "../pages/DebtsPage";
 import SettingsPage from "../pages/SettingsPage";
 import CalendarPage from "../pages/CalendarPage";
 import StatementScannerPage from "../pages/StatementScannerPage";
+import DebtPlannerPage from "../pages/DebtPlannerPage";
 
 export default function AppRouter() {
   return (
@@ -18,6 +19,7 @@ export default function AppRouter() {
           <Route path="/bills" element={<BillsPage />} />
           <Route path="/income" element={<IncomePage />} />
           <Route path="/debts" element={<DebtsPage />} />
+          <Route path="/debt-planner" element={<DebtPlannerPage />} />
           <Route path="/settings" element={<SettingsPage />} />
           <Route path="/timeline" element={<PaydayStrategyPage />} />
           <Route path="/calendar" element={<CalendarPage />} />
